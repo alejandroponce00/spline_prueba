594db5422db98bd58a7af5f4460a98e8bc44a2a9
Alejandro Ponce <alejandroponce00@gmail.com>
2024-11-15 21:01:30 -0300

    primer commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="">La habitacion de Ciro</h1>
+      <p>
+        Esta es una prueba</p>
+      <Spline
+        
+        scene="https://prod.spline.design/x91JD1A6YDkMz8Wc/scene.splinecode" 
+      /> 
+      </div>
+  );
+}
